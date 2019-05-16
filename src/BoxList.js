c2d6e4808b46c1e7b6af68ce143edac345d51869
Jason Matthias <jpmatthias90@gmail.com>
2019-05-16 11:41:30 -0700

    add addBox method

---
+++ src/BoxList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import uuid from 'uuid/v4';
+
+class BoxList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      boxes:[{
+        id: uuid(),
+        width: 100,
+        height: 100,
+        backgroundColor: 'dodgerblue'
+      },],
+    }
+    this.addBox = this.addBox.bind(this);
+  }
+
+  addBox(paramObj) {
+    const newBox = {...paramObj, id: uuid()}
+    this.setState(state => ({
+      boxes: [...state.boxes, newBox]
+    }));
+  }
+
+  render() {
+    const theBoxes = this.state.boxes.map(box => (
+      <Box key={box.id} height={box.height} width={box.width} backgroundColor={box.backgroundColor} />
+    ))
+    return (
+      <div className="BoxList">
+        {theBoxes}
+      </div>
+    );
+  }
+
+}
+
+export default BoxList;
